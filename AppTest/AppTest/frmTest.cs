036e17562d0a74802cde24cf2dddbb109fdda8e2
runningtao <yellowwave@126.com>
2017-05-09 10:11:11 +0800

    首次提交

---
+++ AppTest/AppTest/frmTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppTest
+{
+    public partial class frmTest : Form
+    {
+        public frmTest()
+        {
+            InitializeComponent();
+
+            this.btnTest.Click += BtnTest_Click;
+        }
+
+        private void BtnTest_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello World!");
+            
+        }
+    }
+}
