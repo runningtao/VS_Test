adebd80b0d98e1a156c3e3b9553d54a071051133
runningtao <yellowwave@126.com>
2017-05-09 10:32:32 +0800

    test

--- AppTest/AppTest/frmTest.Designer.cs
+++ AppTest/AppTest/frmTest.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.btnTest = new System.Windows.Forms.Button();
+            this.labShow = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnTest
@@ -40,21 +41,35 @@
             this.btnTest.Text = "Test";
             this.btnTest.UseVisualStyleBackColor = true;
             // 
+            // labShow
+            // 
+            this.labShow.AutoSize = true;
+            this.labShow.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labShow.ForeColor = System.Drawing.Color.Maroon;
+            this.labShow.Location = new System.Drawing.Point(82, 38);
+            this.labShow.Name = "labShow";
+            this.labShow.Size = new System.Drawing.Size(112, 16);
+            this.labShow.TabIndex = 1;
+            this.labShow.Text = "My name is HT";
+            // 
             // frmTest
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.labShow);
             this.Controls.Add(this.btnTest);
             this.Name = "frmTest";
             this.Text = "HelloWorld";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.Label labShow;
     }
 }
 
